4f159a27e96fa49603458b6856df2ef992b9e633
Jungmin Lee <1771199@hansung.ac.kr>
2020-06-29 11:59:25 +0900

    주석 추가

--- DHAS.py
+++ DHAS.py
@@ -18,22 +18,15 @@ import snowboydecoder
 import time
 import os
 import signal
-
 import argparse
 import struct
-#from datetime import datetime
 import datetime
 import numpy as np
 import soundfile
 import queue
-
-
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../binding/python'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../resources/util/python'))
 
-#from porcupine import Porcupine
-#from util import *
 from pulsesensor import Pulsesensor
 # [END import_libraries]
  
@@ -57,35 +50,31 @@ pygame.init()
 pygame.mixer.init()
 
 
-# -----------------------------HotWord----------------------------- #
+# -----------------------------Wake-Up Word----------------------------- #
 
 def interrupt_callback():
     global interrupted
     return interrupted
 
-def callback():
+def callback():  # Wake-Up Word 인식 시 실행되는 콜백 함수.
     global detector
     global canCall
-    if(canCall):
-       canCall = False
-       detector.terminate()
-       global fileName
-       fileName = 'call.mp3'
-       playMP3File()
-       interactive()
+    canCall = False
+    detector.terminate()
+    global fileName
+    # Wake-Up Word 인식 시 "부르셨어요?" 음성 출력.
+    fileName = 'call.mp3'
+    playMP3File()
+    interactive()
 
-def snowBoy():
+def snowBoy(): # Wake-UP Word 인식을 담당하는 함수.
     model = "jangsooya.pmdl"
     global detector
     global canCall
-    detector = snowboydecoder.HotwordDetector(model, sensitivity = 0.5)
+    detector = snowboydecoder.HotwordDetector(model, sensitivity = 0.5) # 민감도를 0.5로 하여 Wake-Up Word detector 생성.
     while True:
-       if(canCall):
-          print(canCall) 
-          detector.start(detected_callback = callback, interrupt_check = interrupt_callback, sleep_time = 0.03)
-       else:
-          print(canCall)
-          detector.terminate()
+        if(canCall):  # 음성이 출력 중일 때 Wake-Up Word를 인식하는 것을 방지.
+            detector.start(detected_callback = callback, interrupt_check = interrupt_callback, sleep_time = 0.03)
 
 #--------------------------------------------------------------#
 #--------------------MIC INPUT --------------------#
@@ -107,9 +96,7 @@ class MicrophoneStream(object):
             input=True, frames_per_buffer=self._chunk,
             stream_callback=self._fill_buffer,
         )
- 
         self.closed = False
- 
         return self
  
     def __exit__(self, type, value, traceback):
@@ -117,7 +104,6 @@ class MicrophoneStream(object):
         self._audio_stream.close()
         self.closed = True
         self._buff.put(None)
-        #self._audio_interface.terminate()
  
     def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
         """Continuously collect data from the audio stream, into the buffer."""
@@ -164,16 +150,14 @@ def listen_print_loop(responses):
         result = response.results[0]
         if not result.alternatives:
             continue
- 
         transcript = result.alternatives[0].transcript
- 
         overwrite_chars = ' ' * (num_chars_printed - len(transcript)) 
         if not result.is_final:
             sys.stdout.write(transcript + overwrite_chars + '\r')
             sys.stdout.flush()
             num_chars_printed = len(transcript)
  
-        else:      # send message to server and recive message
+        else:
            global msg_flag
            global deviceID
            print(transcript + overwrite_chars)
@@ -181,35 +165,29 @@ def listen_print_loop(responses):
            stream.end()
            Send('speech', str(transcript+overwrite_chars))
            msg_flag = False
-           while not msg_flag:                  # msg_flag로 여러 개의 메시지를 처리한 후 더 이상 메시지를 받지 않을 때 처리를 멈춤.
+           while not msg_flag: # msg_flag로 여러 개의 메시지를 처리한 후 더 이상 메시지를 받지 않을 때 처리를 멈춤.
                continue
-                #messageProcessing()
            if re.search(r'\b(exit|quit)\b', transcript, re.I):
                print('Exiting..')
                break
- 
            num_chars_printed = 0
 
 #--------------------Sound--------------------#
  
-def synthesize_text(text):
+def synthesize_text(text):  # Goolge Cloud Speech API를 활용해 text를 음성으로 변환하여 feedback.mp3에 저장.
     """Synthesizes speech from the input string of text."""
     from google.cloud import texttospeech
     done = False
     while not done:
         try:
             client = texttospeech.TextToSpeechClient()
-
             input_text = texttospeech.types.SynthesisInput(text=text)
-
             voice = texttospeech.types.VoiceSelectionParams(
                 language_code='ko-KR',
                 name='ko-KR-Standard-A',
                 ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
-
             audio_config = texttospeech.types.AudioConfig(
                 audio_encoding=texttospeech.enums.AudioEncoding.MP3)
-
             response = client.synthesize_speech(input_text, voice, audio_config)
             with open('feedback.mp3', 'wb') as out:
                 out.write(response.audio_content)
@@ -217,9 +195,7 @@ def synthesize_text(text):
         except:
             pass
 
-    
-
-def playMP3File():
+def playMP3File():  # 음성 출력에 사용하는 함수.
       global micOff
       micOff = True
       global fileName
@@ -233,18 +209,17 @@ def playMP3File():
          continue
       playing = True
       pygame.mixer.music.play()
-      time.sleep(audio.info.length+1)
+      time.sleep(audio.info.length+1) # 음성이 모두 출력될 때까지 대기.
       playing = False
 
-def audioProcess():
+def audioProcess():  # text를 음성으로 변환한 후 재생하는 함수.
     global msg
-    print("audioProcess" + msg)
     synthesize_text(msg)
     playMP3File()
 
 #--------------------Message Processing---------------------#
 
-def messageProcessing():
+def messageProcessing():  # 서버에서 받은 메시지를 유형에 따라 처리하는 함수.
     global msg
     global micOff
     global fileName
@@ -252,53 +227,46 @@ def messageProcessing():
     global listen
     global messageQ
     global canCall
-    #micOff = False
     if not messageQ.qsize() == 0:
-        print("Qsize: ", messageQ.qsize())
         msg = messageQ.get()
         alarmTime = None
-        if 'Output' in msg and '다시' in msg:
-            print("다시")
+        if 'Output' in msg and '다시' in msg:  # 서버에서 사용자의 발화를 정확히 인식하지 못 했을 때 '다시 말씀해 주세요'라는 음성을 출력하고 마이크 입력을 다시 시작.
             fileName = 'repeat.mp3'
             playMP3File()
             msg_flag = True
             listen = False
             micOff = False
 
-        elif 'Output' in msg:
-            print("feedback")
+        elif 'Output' in msg:  # 서버에서 받은 feedback 메시지를 가공하여 음성 출력.
             msg = msg.split('-')[1]
             fileName = 'feedback.mp3'
             audioProcess()
 
-        
-        elif 'No Sleep Data' in msg:
+        elif 'No Sleep Data' in msg:  # 전 날의 수면 데이터가 없을 때, '어제는 몇 시에 주무셨나요?'라는 음성을 출력하고 마이크 입력을 다시 시작.
             fileName = 'NoSleepTime.mp3'
             playMP3File()
             msg_flag = True
             listen = False
             micOff = False
 
-        elif 'Measure Pulse' in msg:
+        elif 'Measure Pulse' in msg:  # 심박수 측정 의도를 확인하였을 때 실행하는 함수. 심박수 측정 시작을 알리는 음성 출력 후 Pulse Sensor로 심박 수를 측정.
             msg_flag = True
             listen = False
             micOff = True
             fileName = 'StartPulse.mp3'
             playMP3File()
-#            heartTest()
             heartBeat()
             canCall = True
         
-        elif 'No Pulse Data' in msg:
+        elif 'No Pulse Data' in msg:  # 심박수 측정 데이터가 없을 때 이를 알림
             fileName = 'NoPulseData.mp3'
             playMP3File()
             canCall = True       
 
-        elif 'End' in msg in msg:
+        elif 'End' in msg in msg:  # 더 이상 받을 메시지가 없을 때 서버에서 받은 평균 시간 기록을 txt로 저장하고 마이크 입력을 중단. 
             msg_flag = True
             listen = False
             micOff = True
-            print(micOff)
             canCall = True
             if alarmTime is not None:
                f = open('TimeAverage.txt', 'w')
@@ -306,28 +274,23 @@ def messageProcessing():
                f.close()
                setAlarmTime()
 
-        elif 'Exist' in msg:
+        elif 'Exist' in msg:  # 심박수 측정 데이터가 존재할 때.
             msg_flag = True            
             listen = False
             micOff = True
             canCall = True
 
-        elif 'Time Average' in msg:
+        elif 'Time Average' in msg: # 평균 시간 데이터를 입력받았을 때 이를 변수에 저장. File I/O에 프로세스가 지체되는 것을 방지하기 위해 이는 'End' 메시지 수신 시 수행.
             alarmTime = msg.split('-')[1]
 
         else:       # 안드로이드 어플리케이션에서 수신한 메시지
-            canCall = False
-            print("canCall1: ", canCall)
             synthesize_text(msg)
-            print("canCall2: ", canCall)
             fileName = 'feedback.mp3'
-            print("canCall3: ", canCall)
             playMP3File()
-            print("canCall4: ", canCall)
             canCall = True
    
 #--------------------main--------------------#
-def interactive():
+def interactive():  # Google Cloud Speech API로 사용자의 발화를 text로 변환하여 서버에 전송.
     global interacting
     global micOff
     global socket_connect
@@ -339,7 +302,6 @@ def interactive():
 
     while not micOff:           # 마이크가 켜져 있는 동안 ... micOff = True: 마이크 꺼짐.
        listen = True
-       print(micOff)
        global client
        global config
        global streaming_config
@@ -349,7 +311,7 @@ def interactive():
            sample_rate_hertz=RATE,
            language_code=language_code)
        streaming_config = types.StreamingRecognitionConfig(
-           config=config,
+           config=config, single_utterance = True,
            interim_results=True)
        global stream
        with MicrophoneStream(RATE, CHUNK) as stream:
@@ -364,10 +326,10 @@ def interactive():
                     done = True
                 except:
                     pass 
-            while listen:           # 원래 True지만 빠져나오기 위해 bool 변수로 설정.
+            while listen:
                 listen_print_loop(responses)
           
-def sendAlarm(meal):
+def sendAlarm(meal): # 서버에 알림 메시지를 전송하는 함수.
     global msg_flag
     global client_socket
     global deviceID
@@ -375,14 +337,11 @@ def sendAlarm(meal):
     msg_flag = False
     while not msg_flag:
         continue
-       # messageProcessing()
 
-
-def Send(tag, body):
+def Send(tag, body): # 소켓 통신으로 메시지를 가공하여 서버에 전송.
     global client_socket
     global deviceID
     global info
-    print("Send")
     if body == "alarm":
         send = tag + "-" + deviceID
     else:
@@ -395,7 +354,6 @@ def Send(tag, body):
             client_socket.sendall(length.to_bytes(4, byteorder="little"))
             client_socket.sendall(data)
             done = True
-            print("Done")
         except:
             try:
                 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -405,16 +363,8 @@ def Send(tag, body):
                 client_socket.sendall(length.to_bytes(4, byteorder="little"))
                 client_socket.sendall(id)
             except: pass
-            
 
-def heartTest():
-   Send('pulse','80')
-   global msg_flag
-   while not msg_flag:
-      continue
-     # messageProcessing()
-
-def heartBeat():
+def heartBeat(): # Pulse Sensor로 심박수를 측정하는 함수.
     global fileName
     global deviceID
     p = Pulsesensor()
@@ -424,39 +374,33 @@ def heartBeat():
     tryNum = 0
     Done = False
     startTime = time.time()
-    while not Done:
+    while not Done: # 측정이 정확하게 될 때까지 반복.
        nowTime = time.time()
-       while int(nowTime - startTime) <= 12:
+       while int(nowTime - startTime) <= 12: # 12초 동안 측정.
             bpm = p.BPM
-            print(str(bpm))
-            if bpm >= 60 and bpm < 84:
-                print("IN")
+            if bpm >= 60 and bpm < 84:  # Pulse Sensor가 비정상적인 값을 인식할 때, 평균 계산에 적용하지 않기 위함.
                 bpmSum += bpm
-                count += 1
+                count += 1 # 정확한 측정이 몇 번 수행되었는지 확인.
             nowTime = time.time()
             continue
-       if count is not 0:
-          p.stopAsyncBPM()
-          value = bpmSum / count
-          Done = True
-          intvalue = str(round(value, 1)).split('.')[0]
-          print("intvalue: " + intvalue)
+       if count is not 0: # 정확한 측정이 한 번이라도 수행되었을 때.
+          p.stopAsyncBPM() # 측정 중단.
+          value = bpmSum / count # 평균 값 계산.
+          Done = True # 측정 완료를 알림.
+          intvalue = str(round(value, 1)).split('.')[0] # 소수점 첫째 자리에서 반올림 하여 int형으로 변환.
        else:
-          fileName = "PulseAgain.mp3"
+          fileName = "PulseAgain.mp3" # 측정이 정확하지 않을 때 다시 측정하겠다는 알림 출력.
           playMP3File()
           tryNum +=1
           startTime = time.time()
-    Send('pulse', intvalue)
+    Send('pulse', intvalue) # 측정 값을 서버에 전송.
     global msg_flag
- #   msg_flag = False
     while not msg_flag:
        continue
-       #messageProcessing()
-
 
 #-------------------------------Thread Method--------------------------------#
 
-def Recieve():
+def Recieve(): # 소켓 통신으로 서버 메시지를 수신하는 함수.
     global client_socket
     global fileName
     global deviceID
@@ -474,22 +418,21 @@ def Recieve():
     client_socket.sendall(length.to_bytes(4, byteorder="little"))
     client_socket.sendall(id)
     sendID = True
-    while True:
+    while True:  # 데이터의 유실을 막기 위해 Decoding을 진행한 후 Queue에 저장.
         data = client_socket.recv(4)
         length = int.from_bytes(data, "little")
         data = client_socket.recv(length)
         message = data.decode("utf-8")
-        print("server message: "+ message)
         messageQ.put(message)
 
-def dataQueue():
+def dataQueue(): # Queue에 메시지가 담겨 있을 때, 이를 처리함.
    global messageQ
    global msg_flag
    messageQ = queue.Queue()
    while True:
       messageProcessing()
 
-def timer():
+def timer(): # TimeAverage.txt의 평균 시간에 따른 식전 약 복용 정보 요청과 일정 시간의 심박수 측정 확인 알림을 서버에 전송.
     global baTime
     global laTime
     global daTime
@@ -519,7 +462,6 @@ def timer():
           if now.hour == daTime.hour and now.minute == daTime.minute:
              if d_oneTimeOnly is not True:
                 sendAlarm("dinner")
-                print("check")
                 d_oneTimeOnly = True 
           if now.hour == 15  and now.minute == 30:
              if h_oneTimeOnly is not True:
@@ -527,15 +469,11 @@ def timer():
                 msg_flag = False
                 while not msg_flag:
                    continue
-                # messageProcessing()
                 h_oneTimeOnly = True
         
-
-
-
 #----------------------------------------------------------------------------------------------------#
 
-def initDevice():
+def initDevice():  # 전원 열결 시 txt 파일로 Device ID를 설정.
     global deviceID
 #    global client_socket
     global playing
@@ -544,7 +482,7 @@ def initDevice():
     deviceID = f.readline()
     f.close()
 
-def setAlarmTime():
+def setAlarmTime():  # TimeAverage.txt 파일에 담긴 평균 시간 데이터를 활용하여 식사시간 평균 30분 전의 알림 시간을 설정.
     global baTime
     global laTime
     global daTime
@@ -607,9 +545,7 @@ def setAlarmTime():
        dhour = str(int(dhour) - 1)
     else: dmin = str(int(dmin)-30)
     daTime = datetime.time(int(dhour), int(dmin))
-    print(dhour + ":" +dmin)
     alarmTime = None
-
 
 if __name__ == '__main__':
     initDevice()
